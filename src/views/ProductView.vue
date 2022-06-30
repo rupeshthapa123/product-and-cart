e842b99105b0707a132c554637d7556d9a1fe6c6
Rupesh <rupesh.thapa2050@gmail.com>
2022-06-30 22:46:39 +0545

    vue js practice project

---
+++ src/views/ProductView.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <div class="card-container">
+            <ProductCardVue
+            v-for="(product, index) in inventory.slice(0,6)"
+            :key="product.id"
+            class="card"
+            :index="index"
+            :product="product"
+            :addToCart="addToCart"
+        />
+        </div>
+    </main>
+</template>
+
+<script>
+import ProductCardVue from '@/components/ProductCard.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCardVue
+  }
+}
+</Script>
